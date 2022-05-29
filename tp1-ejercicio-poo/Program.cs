9e61fe7879b28c4e07138386e78493bd7b903ab5
Benjamin Rossi <benjarossi9719@gmail.com>
2022-05-29 17:56:30 -0300

    Initial commit.

---
+++ tp1-ejercicio-poo/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tp1_ejercicio_poo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
