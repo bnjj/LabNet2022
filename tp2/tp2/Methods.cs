47d88ead1a31f3d9fef1e16f9c9d1b1dfd4a10c0
Benjamin Rossi <benjarossi9719@gmail.com>
2022-06-01 12:48:28 -0300

    punto 1 + punto2

---
+++ tp2/tp2/Methods.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tp2
+{
+    public static class Methods
+    {
+
+        public static int Divide100By(this int divisor)
+        {
+            try
+            {
+               
+                Console.WriteLine("Resultado {0}", 100/divisor);
+                return 100/divisor;
+
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+
+            }
+            finally
+            {
+                Console.WriteLine("Programa termino");
+                
+            }
+            return divisor;
+
+        }
+        public static int Division()
+        {
+
+            try
+            {
+                Console.WriteLine("Ingrese un divisor");
+                int dividend = int.Parse(Console.ReadLine());
+                Console.WriteLine("Ingrese un dividendo");
+                int divisor = int.Parse(Console.ReadLine());
+                Console.WriteLine("Resultado de la operacion : {0}", dividend / divisor);
+                return dividend / divisor;
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine("Solo Chuck Norris puede dividir en 0 mi rey. Mensaje de excepcion --> "+ex.Message);
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Seguro ingreso una letra o no ingreso nada ! -_- ");
+            }
+             finally { 
+                Console.WriteLine("Programa termino.");
+            }
+            return 0;
+        }
+
+    }
+}
---
+++ tp2/tp2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Punto 1 
+            Console.WriteLine("Punto 1");
+            int number = 0;
+
+            // Console.Write("Numero ingresado {0}\n",number);
+            int resultadoPunto1 = number.Divide100By();
+
+          
+            Console.ReadLine();
+
+            //Punto2
+            Console.WriteLine("Punto 2 Dividamos");
+
+
+            int resultadoPunto2 = Methods.Division();
+          
+            Console.ReadLine();
+
+
+
+        }
+
+    }
+}
